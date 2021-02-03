8638b2d1efa4dc4306344376e0befbeff40fb773
Hiram Guerrero <hiram.guerrero@hiram.guerrero>
2021-02-02 21:42:14 -0600

    Add Bundle queries and mutations

--- utils/api/resolvers.ts
+++ utils/api/resolvers.ts
@@ -11,11 +11,20 @@ export const resolvers = {
     },
     feeds: (parent, args, {prisma}) => {
       return prisma.feed.findMany();
-    }
+    },
+    bundle: (parent, {data: {id}}, {prisma}) => prisma.bundle.findUnique({where: {id}}),
+    bundles: (parent, args, {prisma}) => prisma.bundle.findMany(),
   },
   Mutation: {
     createFeed: async (parent, {data}, {prisma, user}) => {
       const result = await prisma.feed.create({
+        data: {...data},
+      });
+
+      return result;
+    },
+    createBundle: async (parent, {data}, {prisma, user}) => {
+      const result = await prisma.bundle.create({
         data: {...data},
       });
 
